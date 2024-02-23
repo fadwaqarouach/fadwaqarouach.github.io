49f0aec3af98ec9a326793f9829d25e65fc91094
Qarouach <fqarouach@gmail.com>
2024-02-23 11:13:28 +0100

    premier envoi sur internet

---
+++ README.md
@@ -0,0 +1,35 @@
+# Event Calendar in Django
+
+
+# Demo
+![image](x)
+
+
+## How To Setup
+```
+git 
+```
+```
+cd event-calendar
+```
+```
+python3 -m venv venv
+```
+```
+source venv/bin/activate
+```
+```
+pip install -r requirements.txt
+```
+```
+python manage.py makemigrations
+```
+```
+python manage.py migrate
+```
+```
+python manage.py createsuperuser
+```
+```
+python manage.py runserver
+```
---
+++ static/js/index.js
@@ -0,0 +1,16 @@
+ //click event
+
+ const eventAganda = document.querySelector(".fc-event");
+
+ eventAganda.addEventListener("click",()=>{
+	eventAganda.style.background="#add8e6";
+ });
+//---------------------------------------------------------------------------------------------------------------------------
+ //Mouse Events
+
+ /*const mousemoves=document.querySelector(".mousemove");
+
+ window.addEventListener("mousemove", (e)=>{
+    mousemoves.style.left=e.pageX+"px";
+    mousemoves.style.left=e.pageY+"px";
+ });*/
